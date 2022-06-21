93f1432cc9d33fd79e2e2f2e20cb50aed2550e5a
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-06-21 21:43:53 +0300

    style: renew footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__text">Разрабатывается при изучении фронтенда в&nbsp;Яндекс.Практикуме</p>
-      <p className="footer__text">© 2022 novvember</p>
+      <p className="footer__text">Разработано во время обучения на фронтенд-разработчика в&nbsp;Яндекс.Практикуме</p>
+      <p className="footer__text"><a href="https://github.com/novvember/yet-another-sorting-visualization">Репозиторий этого приложения на GitHub</a></p>
+      <p className="footer__text">© 2022 <a href="https://github.com/novvember">novvember</a></p>
     </footer>
   );
 }
