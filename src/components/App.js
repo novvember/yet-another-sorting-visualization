e75a8dcf62b1e1295a70e5d1349c5e072953c34a
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-06-21 13:29:36 +0300

    feat: add Header

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "./Header";
+
+
+function App() {
+
+  return (
+    <div className="content">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+
+  return (
+    <header class="header">
+      <h1 class="header__title">Yet Another Sorting Visualization</h1>
+      <p class="header__text">Visualization of basic sorting algorithmes</p>
+      <p class="header__text">Being developed while learning frontend at Yandex.Practicum in 2022</p>
+    </header>
+  );
+}
+
+export default Header;
