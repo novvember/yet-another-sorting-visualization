a97ed7f808b0ccae0299387b405d8607098e9842
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-06-21 14:39:16 +0300

    feat: make GeneralControls

---
+++ src/components/GeneralControls.js
@@ -0,0 +1,11 @@
+function GeneralControls() {
+  return (
+    <div className="general-controls">
+      <button type="button">🔀 Перемешать</button>
+      <button type="button">📈 Сортировать по возрастающей</button>
+      <button type="button">📉 Сортировать по убывающей</button>
+    </div>
+  );
+}
+
+export default GeneralControls;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,9 @@
+import GeneralControls from "./GeneralControls";
+
 function Main() {
   return (
     <main>
-      <div className="general-controls">
-        <button type="button">Перемешать</button>
-        <button type="button">Сортировать по возрастающей</button>
-        <button type="button">Сортировать по убывающей</button>
-      </div>
+      <GeneralControls />
 
       <div className="playground">
         <ul className="field">
