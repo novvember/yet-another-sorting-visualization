e981a6d00869b0212e9dbf0c20365b1b128cc9df
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-06-21 15:44:19 +0300

    feat: update styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="footer">
       <p className="footer__text">Разрабатывается при изучении фронтенда в&nbsp;Яндекс.Практикуме</p>
-      <p className="footer__text">2022</p>
+      <p className="footer__text">© 2022 novvember</p>
     </footer>
   );
 }
