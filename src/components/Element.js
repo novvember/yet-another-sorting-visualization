036f3a6ed731337d46959437d2b1edca6597b481
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-06-26 01:25:11 +0300

    style: change classname assignment in Element

--- src/components/Element.js
+++ src/components/Element.js
@@ -1,8 +1,11 @@
 function Element({height, color, isActive, isDone}) {
+  let className = 'element';
+  isActive && (className += " element_status_active");
+  isDone && (className += " element_status_done");
 
   return (
     <li
-      className={"element" + (isActive ? " element_status_active" : '') + (isDone ? " element_status_done" : '')}
+      className={className}
       style={{
         height: `${height}%`,
         backgroundColor: color,
