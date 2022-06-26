019942e978a9478aea4b3cce0f6a55175711e4e0
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-06-26 23:27:58 +0300

    refactor: extract Form

---
+++ src/components/Form.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+function Form({ title, hasButton, min, max, step, defaultValue, onChange, onSubmit }) {
+  const errorClass = 'form__number-input_error';
+
+  const [value, setValue] = React.useState(defaultValue);
+  const [isValid, setIsValid] = React.useState(true);
+
+  function checkIsValid(value) {
+    return value >= min && value <= max;
+  }
+
+  function handleChange(event) {
+    const currentValue = +event.target.value;
+    setValue(currentValue);
+    if ( checkIsValid(currentValue) ) {
+      setIsValid(true);
+      if (onChange) onChange(currentValue);
+    } else {
+      setIsValid(false);
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if ( checkIsValid(value) && onSubmit) onSubmit(value);
+  }
+
+  return (
+    <form className="input form" onSubmit={handleSubmit}>
+      <label className="form__label">
+        <span className="form__title">{title}</span>
+        <input
+          className="form__range-input"
+          type="range"
+          min={min}
+          max={max}
+          step={step}
+          value={value}
+          onChange={handleChange}
+        />
+      </label>
+      <input
+        className={'form__number-input ' + (!isValid ? errorClass : '')}
+        type="number"
+        minLength={min}
+        maxLength={max}
+        value={value}
+        onChange={handleChange}
+      />
+
+      {hasButton && (
+        <button className="form__button" type="submit" aria-label="Сохранить">
+          🡢
+        </button>
+      )}
+    </form>
+  );
+}
+
+export default Form;
--- src/components/GeneralControls.js
+++ src/components/GeneralControls.js
@@ -1,51 +1,32 @@
 import React from "react";
 
-import NumberInput from "./NumberInput";
+import Form from "./Form";
 
 function GeneralControls({barsNumber, onBarsChange, onShuffle, onRunSorting, delay, onDelayChange}) {
 
-  const [barsValue, setBarsValue] = React.useState(barsNumber);
-
-  function handleBarsSubmit(event) {
-    event.preventDefault();
-    onBarsChange(barsValue);
-  }
-
-  function handleDelayChange(event) {
-    onDelayChange(event.target.value);
-  }
-
   return (
     <div className="general-controls">
-      <form className="input input_type_form">
-        <label>
-          <span>Замедление</span>
-          <input
-            className="input input_type_form-input input_type_range"
-            type="range"
-            name="delay"
-            min="50"
-            max="1000"
-            step="50"
-            onChange={handleDelayChange}
-            value={delay}
-          />
-          <span>{delay}</span>
-        </label>
-      </form>
+      <Form
+        title="Замедление"
+        hasButton={false}
+        min="50"
+        max="1000"
+        step="50"
+        defaultValue={delay}
+        onChange={onDelayChange}
+        onSubmit={null}
+      />
 
-      <form className="input input_type_form" onSubmit={handleBarsSubmit}>
-        <label>
-          <NumberInput
-            minLength="10"
-            maxLength="1000"
-            defaultValue={barsValue}
-            onChange={setBarsValue}
-          />
-          <span> столбиков</span>
-        </label>
-        <button type="submit" className="input input_type_form-button">❯</button>
-      </form>
+      <Form
+        title="Столбики"
+        hasButton={true}
+        min="10"
+        max="1000"
+        step="10"
+        defaultValue={barsNumber}
+        onChange={null}
+        onSubmit={onBarsChange}
+      />
 
       <button type="button" className="input" onClick={onShuffle}>🔀 Перемешать</button>
       <button type="button" className="input input_important" onClick={onRunSorting}>⏭ Запустить</button>
