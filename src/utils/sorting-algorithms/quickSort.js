20e26459bcc157eb64d987cfd30554ee702a9a6f
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-08-18 16:55:25 +0300

    feat: добавить алгоритм быстрой сортировки (первая итерация)

---
+++ src/utils/sorting-algorithms/quickSort.js
@@ -0,0 +1,62 @@
+export default async function quickSort({
+  array, // массив для сортировки
+  render, // отобразить состояние без задержки: render()
+  renderWithDelay, // отобразить состояние с задержкой: await renderWithDelay()
+  setActive, // отметить элементы массива как текущие: setActive(a, b)
+  clearActive, // Убрать отметку у активных элементов: clearActive(a, b)
+  setDone, // отметить элементы массива как выполненные: setDone(doneElement)
+  clearStatuses, // убрать любые отметки с элементов: clearStatuses()
+  isStopped, // параметр для проверки остановки пользователем: if (isStopped.value)
+}) {
+
+  async function sort(startIndex, endIndex) {
+    const pivot = array[startIndex];
+
+    setActive(pivot);
+    await renderWithDelay();
+
+    for (let i = startIndex; i < endIndex; i++) {
+      const element = array[i];
+      const pivotIndex = array.indexOf(pivot);
+      if (i === pivotIndex) continue;
+
+      setActive(element);
+      await renderWithDelay();
+
+      if (element.value < pivot.value) {
+        if (i >= pivotIndex) move(array, i, array.indexOf(pivot));
+      } else {
+        if (i < pivotIndex) move(array, i, array.indexOf(pivot) + 1);
+      }
+
+      await renderWithDelay();
+      clearActive(element);
+    }
+    clearActive(pivot);
+    setDone(pivot);
+
+    const pivotIndex = array.indexOf(pivot);
+    const lowerArrayIndexes = [startIndex, pivotIndex];
+    const higherArrayIndexes = [pivotIndex + 1, endIndex];
+
+    if (lowerArrayIndexes[1] - lowerArrayIndexes[0] > 1) await sort(...lowerArrayIndexes);
+    if (higherArrayIndexes[1] - higherArrayIndexes[0] > 1) await sort(...higherArrayIndexes);
+  }
+
+
+  console.log('quick sort started');
+
+  await sort(0, array.length);
+
+  console.log('quick sort done');
+}
+
+function move(array, fromIndex, toIndex) {
+  array.splice(toIndex, 0, array.splice(fromIndex, 1)[0]);
+  return true;
+}
+
+function swap(array, indexA, indexB) {
+  [ array[indexA], array[indexB] ] = [ array[indexB], array[indexA] ];
+  return true;
+}
--- src/utils/sorting-algorithms/sortingAlgorithms.js
+++ src/utils/sorting-algorithms/sortingAlgorithms.js
@@ -1,10 +1,16 @@
 import bubbleSort from "./bubbleSort";
+import quickSort from "./quickSort"
 
 const sortingAlgorithms = [
   {
     id: 'bubbleSort',
     name: '... пузырьком',
     f: bubbleSort,
+  },
+  {
+    id: 'quickSort',
+    name: 'Быстрая...',
+    f: quickSort,
   },
 ]
 
